84d4d8e33b5a31de8f36aa3690d3ef9b21b871cf
eduardoarad <araujocarlos893@gmail.com>
2023-09-20 11:43:18 -0300

    Estrutura Visual

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,19 @@
 import type { AppProps } from 'next/app'
-import { Roboto } from 'next/font/google'
+import Image from 'next/image'
 
-const roboto = Roboto({
-  weight: ['400', '700'],
-  subsets: ['latin'],
-})
+import { globalStyles } from '../styles/global'
+import { Container, Header } from '../styles/pages/app'
+import logoImg from '../assets/logo.svg'
+
+globalStyles()
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <main className={roboto.className}>
+    <Container>
+      <Header>
+        <Image {...logoImg} />
+      </Header>
       <Component {...pageProps} />
-    </main>
+    </Container>
   )
 }
